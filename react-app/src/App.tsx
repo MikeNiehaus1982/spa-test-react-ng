072a03d51186a54c6b0191c918a13c8e2c1ee92b
mike.niehaus <mike.niehaus@gdata.de>
2022-06-05 11:04:44 +0200

    Add images and iframe to react-app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -10,27 +10,27 @@ export default function App({ name }) {
         <nav>
           <ul>
             <li>
-              <Link to="/react">Home</Link>
+              <Link to="/react/image">Image</Link>
             </li>
             <li>
-              <Link to="/react/about">About</Link>
+              <Link to="/react/gdataimage">G DATA Image</Link>
             </li>
             <li>
-              <Link to="/react/users">Users</Link>
+              <Link to="/react/dukenukemminigame">Duke Nukem Minigame</Link>
             </li>
           </ul>
         </nav>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/react/about">
-            <About />
+          <Route path="/react/gdataimage">
+            <GDataImage />
           </Route>
-          <Route path="/react/users">
-            <Users />
+          <Route path="/react/dukenukemminigame">
+            <DukeNukemMiniGame />
           </Route>
-          <Route path="/react">
-            <Home />
+          <Route path="/react/image">
+            <Image />
           </Route>
         </Switch>
       </div>
@@ -38,7 +38,7 @@ export default function App({ name }) {
   );
 }
 
-function Home() {
+function Image() {
   return (
     <div>
       <img src="https://create-react-app.dev/img/logo-og.png" width="100%" height="100%"></img>
@@ -46,10 +46,17 @@ function Home() {
   )
 }
 
-function About() {
-  return <h2>About</h2>;
+function GDataImage() {
+  return (
+    <div>
+      <img src="https://www.gdata.de/fileadmin/web/general/images/wallpaper/MeineDaten_Screen_1920x1080_Logo.jpg" width="100%" height="100%"></img>
+    </div>
+  )
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function DukeNukemMiniGame() {
+  return (
+      <iframe src="https://archive.org/embed/DukeNukem" width="560" height="384"></iframe>
+  )
+  //return <h2>Users</h2>;
 }
